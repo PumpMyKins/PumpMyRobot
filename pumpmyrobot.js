7d661736bd252c6bee648bddc611b53bdb9c8caf
Clem Fern <clemfern456@gmail.com>
2020-10-30 18:37:07 +0100

    Add ready & disconnected bot event

--- pumpmyrobot.js
+++ pumpmyrobot.js
@@ -5,13 +5,28 @@ process.on('unhandledRejection', (reason) => {
 
 try {
     const discord = require('discord.js');
-    const client = new discord.Client();
+    const bot = new discord.Client();
     
     console.log("Starting PumpMyRobot\nNode version: " + process.version + "\nDiscord.js version: " + discord.version);
 
     const config = require('./config').getConfig();
 
-    client.login(config.TOKEN);
+    bot.on("ready", function () {
+        bot.user.setPresence({
+            activity: {
+                name: "PumpMyKins"
+            }
+        });        
+    });
+
+    bot.on("disconnected", function () {
+
+        console.log("Disconnected!");
+        process.exit(1);
+    
+    });
+
+    bot.login(config.TOKEN);
 
 } catch (e){
 	console.log(e.stack);

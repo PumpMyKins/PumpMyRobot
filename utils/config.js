f0502b225960d8de91261b648de6e39d2cced619
Clem Fern <clemfern456@gmail.com>
2020-11-17 22:00:55 +0100

    Move config.js in utils package

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 var Config = {};
 try {
-    Config = require("./config.json");
+    Config = require("../config.json");
 } catch (e){
     console.log("Please create a copy of config.example.json named config.json");
     console.log(e.stack)

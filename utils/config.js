aa239a7b8414d98f559abbc077eaf469c169bcf7
Clem Fern <clemfern456@gmail.com>
2020-11-18 12:17:07 +0100

    Add db url field in config

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,7 @@
 var Config = {};
 try {
     Config = require("../config.json");
+    Config.mongoUrl = "mongodb+srv://" + Config.mongo.username + ":" + Config.mongo.password + "@" + Config.mongo.host + "/" + Config.mongo.database + "?retryWrites=true&w=majority";
 } catch (e){
     console.log("Please create a copy of config.example.json named config.json");
     console.log(e.stack)

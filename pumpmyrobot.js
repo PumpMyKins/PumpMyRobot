e1f0d58300da5d94c09b6458aaf3199a29535ad5
Clem Fern <clemfern456@gmail.com>
2020-11-18 12:19:58 +0100

    code clean (mongoose connection, guild verification, creating & loading guild properties)

--- pumpmyrobot.js
+++ pumpmyrobot.js
@@ -5,72 +5,104 @@ process.on('unhandledRejection', (reason) => {
 
 process.on('SIGTERM', () => {
     console.info('SIGTERM signal received.');
-  });
+});
 
 try {
-    const discord = require('discord.js');
-    const bot = new discord.Client();
+    const config = require('./utils/config').getConfig();
+
+    const PumpMyMongoose = require('./utils/pmmongoose');
+    var connection = PumpMyMongoose.connection(config.mongoUrl); 
+    connection.on('error', console.error.bind(console, "Mongo connexion ERROR")); 
+    connection.once('open', function (){
+        console.log("Mongo OK..."); 
+
+        const discord = require('discord.js');
+        const bot = new discord.Client();
+
+        console.log("Starting " + config.bot.name);
+        console.log("Node version: " + process.version);
+        console.log("Discord.js version: " + discord.version);
     
-    const config = require('./utils/config.js').getConfig();
+        bot.on("ready", function () {
+            bot.user.setUsername(config.bot.name);
+            bot.user.setPresence({
+                activity: {
+                    name: config.bot.default_activity
+                }
+            });  
 
-    console.log("Starting " + config.bot.name);
-    console.log("Node version: " + process.version);
-    console.log("Discord.js version: " + discord.version);
-
-    bot.on("ready", function () {
-        bot.user.setUsername(config.bot.name)
-        bot.user.setPresence({
-            activity: {
-                name: config.bot.default_activity
-            }
-        });  
-        
-        leaveUnallowedGuilds(config,bot)
-        
-        //after checking, register all others events
-        registerAllOthersEvents(config,bot)
-
-    });
-
-    bot.on("disconnected", function () {
-
-        console.log("Disconnected!");
-        process.exit(1);
+            setupBot(config,PumpMyMongoose,bot);
     
-    });
+        });
+    
+        bot.on("disconnected", function () {
+    
+            console.log("Disconnected!");
+            process.exit(1);
+        
+        });
+    
+        bot.login(config.TOKEN);
 
-    bot.login(config.TOKEN);
-
+    }); 
 } catch (e){
 	console.log(e.stack);
 	console.log(process.version);
 	process.exit();
 }
 
-function registerAllOthersEvents(config,bot) {
-    
+function setupBot(config,pumpmymongoose,bot) {
+
+    setupGuilds(config,pumpmymongoose,bot);
+
     //joined a server
     bot.on("guildCreate", guild => {
         console.log("Joined a new guild[" + guild.id + "] : " + guild.name);
-        leaveGuildIfUnallowed(config,guild)
+        setupGuild(config,pumpmymongoose,guild);
     })
 
 }
 
-function leaveUnallowedGuilds(config,bot) {
-    // allowed guilds checking
-    const guilds = bot.guilds.cache
+function setupGuilds(config,pumpmymongoose,bot) {
+    const guilds = bot.guilds.cache;
     guilds.forEach(guild => {
-        leaveGuildIfUnallowed(config,guild)
+        setupGuild(config,pumpmymongoose,guild);
     });
+}
+
+function setupGuild(config,pumpmymongoose,guild) {
+    if(!leaveGuildIfUnallowed(config,guild)){
+        // setup guild
+        pumpmymongoose.Guild.findOne({'_gid': guild.id}, function(err, g) {
+            if(g && !err){
+                console.log("GuildConfig[" + g._gid + "] find...");
+            }else{
+                console.log("GuildConfig not find...");
+                var GuildConfig = new pumpmymongoose.Guild();
+                GuildConfig._gid = guild.id;
+                GuildConfig.command_prefix = config.bot.default_command_prefix;
+                GuildConfig.save(function(err1){
+                    if(err1){
+                        console.log("new GuildConfig save ERROR");
+                        console.log(err1);
+                    }else{
+                        console.log("new GuildConfig saved !");
+                    }
+                });
+            }
+        })
+    }
 }
 
 function leaveGuildIfUnallowed(config,guild) {
     if(!config.security.allowed_guilds_id.includes(guild.id)){
         console.log("guild[" + guild.id + "] not allowed ! leaving...");
-        guild.leave()
-    }else{
-        //debug
-        console.log("DEBUG guild[" + guild.id + "] allowed !");
+        guild.leave();
+        return true;
     }
+    
+    //debug
+    console.log("DEBUG guild[" + guild.id + "] allowed !");
+    return false;
+    
 }

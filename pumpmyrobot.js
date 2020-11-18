c6850dbe583f664c6371beae64485a4025417fc3
Clem Fern <clemfern456@gmail.com>
2020-11-18 14:34:57 +0100

    pumpmyrobot, code clean up, add guild_manager module

--- pumpmyrobot.js
+++ pumpmyrobot.js
@@ -11,10 +11,11 @@ try {
     const config = require('./utils/config').getConfig();
 
     const PumpMyMongoose = require('./utils/pmmongoose');
-    var connection = PumpMyMongoose.connection(config.mongoUrl); 
+    const connection = PumpMyMongoose.connection(config.mongoUrl); 
     connection.on('error', console.error.bind(console, "Mongo connexion ERROR")); 
     connection.once('open', function (){
         console.log("Mongo OK..."); 
+
 
         const discord = require('discord.js');
         const bot = new discord.Client();
@@ -53,56 +54,12 @@ try {
 
 function setupBot(config,pumpmymongoose,bot) {
 
-    setupGuilds(config,pumpmymongoose,bot);
-
+    const GuildManager = require('./utils/guild_manager');
+    GuildManager.setupAll(config,pumpmymongoose,bot);
     //joined a server
     bot.on("guildCreate", guild => {
         console.log("Joined a new guild[" + guild.id + "] : " + guild.name);
-        setupGuild(config,pumpmymongoose,guild);
-    })
-
-}
-
-function setupGuilds(config,pumpmymongoose,bot) {
-    const guilds = bot.guilds.cache;
-    guilds.forEach(guild => {
-        setupGuild(config,pumpmymongoose,guild);
+        GuildManager.setup(config,pumpmymongoose,guild);
     });
-}
 
-function setupGuild(config,pumpmymongoose,guild) {
-    if(!leaveGuildIfUnallowed(config,guild)){
-        // setup guild
-        pumpmymongoose.Guild.findOne({'_gid': guild.id}, function(err, g) {
-            if(g && !err){
-                console.log("GuildConfig[" + g._gid + "] find...");
-            }else{
-                console.log("GuildConfig not find...");
-                var GuildConfig = new pumpmymongoose.Guild();
-                GuildConfig._gid = guild.id;
-                GuildConfig.command_prefix = config.bot.default_command_prefix;
-                GuildConfig.save(function(err1){
-                    if(err1){
-                        console.log("new GuildConfig save ERROR");
-                        console.log(err1);
-                    }else{
-                        console.log("new GuildConfig saved !");
-                    }
-                });
-            }
-        })
-    }
-}
-
-function leaveGuildIfUnallowed(config,guild) {
-    if(!config.security.allowed_guilds_id.includes(guild.id)){
-        console.log("guild[" + guild.id + "] not allowed ! leaving...");
-        guild.leave();
-        return true;
-    }
-    
-    //debug
-    console.log("DEBUG guild[" + guild.id + "] allowed !");
-    return false;
-    
 }

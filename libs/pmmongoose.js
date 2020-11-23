91c5f479472bcd8a41ac29da02e66897273d79c6
Clem Fern <clemfern456@gmail.com>
2020-11-23 11:09:28 +0100

    pumpmymongoose clean up

--- libs/pmmongoose.js
+++ libs/pmmongoose.js
@@ -7,16 +7,8 @@ var RoleSchema = new Schema({
     weight : { type: Number, required : true}
 });
 
-var RoleReacSchema = new Schema({
-    guild_id : { type: String, required : true},
-    channel_id : { type: String, required : true},
-    message_id : { type: String, required : true},
-    reac_id : { type: String, required : true},
-    role_ids : [String]
-}).index({ guild_id: 1, channel_id: 1, message_id: 1, reac_id: 1 },{ unique: true });
-
 var GuildSchema = new Schema({
-    id : { type: String, unique : true, index : true, required : true}, 
+    _id : { type: String, required : true}, 
     command_prefix : { type: String, default: '.p'},
     special_channels : {
         welcome_channel_id : { type: String, default: 'none'},
@@ -27,18 +19,25 @@ var GuildSchema = new Schema({
         untrack_channels_id : [String],
         untrack_categories_id : [String],
     },
-    roles : [RoleSchema],
     archives : {
         public_categorie_id : { type: String, default: 'none'},
         private_categorie_id : { type: String, default: 'none'}
     },
 });
 
-var StalkerSchema = new Schema({
+var RoleReacSchema = new Schema({
+    guild_id : {type : String, ref: 'Guild', required : true},
+    channel_id : { type: String, required : true},
+    message_id : { type: String, required : true},
+    reac_id : { type: String, required : true},
+    role_ids : {type: [String], required: true}
+}).index({ guild_id: 1, channel_id: 1, message_id: 1, reac_id: 1 },{ unique: true });
+
+var StreamerSchema = new Schema({
     streamer_id : { type: String, unique : true, index : true, required : true}
 });
 
-const StalkerModel = mongoose.model('stalkers', StalkerSchema);
+const StreamerModel = mongoose.model('stalkers', StreamerSchema);
 const GuildModel = mongoose.model('guilds', GuildSchema);
 const RoleReacModel = mongoose.model('rolereacs', RoleReacSchema);
 const RoleModel = mongoose.model('roles', RoleSchema);
@@ -50,7 +49,7 @@ function Connection(url) {
 
 exports.mongoose = mongoose
 exports.connection = Connection
-exports.Stalker = StalkerModel
+exports.Streamer = StreamerModel
 exports.Guild = GuildModel
 exports.RoleReac = RoleReacModel
 exports.Role = RoleModel

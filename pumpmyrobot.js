90d6003a6437a6fb8bd138f6cc4125786e569adc
Clem Fern <clemfern456@gmail.com>
2020-12-21 11:32:09 +0100

    last commit

--- pumpmyrobot.js
+++ pumpmyrobot.js
@@ -20,7 +20,7 @@ try {
 
 
         const discord = require('discord.js');
-        const bot = new discord.Client({partials : ['MESSAGE', 'USER', 'GUILD_MEMBER',  'CHANNEL', 'REACTION']});
+        const bot = new discord.Client({partials : ['MESSAGE', 'GUILD_MEMBER',  'CHANNEL', 'REACTION']});
 
         logger.info("Starting " + config.bot.name);
         logger.info("Node version: " + process.version);
@@ -71,6 +71,104 @@ function setupBot(config,pumpmymongoose,bot) {
         StreamSniper.setup(config,pumpmymongoose,bot);
     });
 
+    // role reac assign module
+    const RoleReac = require('./utils/role_reac');
+    RoleReac.setLogger(logger)
+    RoleReac.setupAll(pumpmymongoose,GuildManager,bot); // checking reactions to add role
+
+    //leaved a server
+    bot.on("guildDelete", guild => {
+        logger.info("Leaved guild[" + guild.id + "] : " + guild.name);
+        logger.warn("Deleting guild[" + guild.id + "]'s data : " + guild.name);
+        // remove data function
+        GuildManager.delete(pumpmymongoose,guild);
+        RoleReac.delete(pumpmymongoose,guild);
+    });
+
+    //Role Reac messageReactionAdd
+    // autoremove member reaction after role assign
+    // if user already have role, juste remove it
+    bot.on("messageReactionAdd", async (messageReaction, user) => {
+
+        if (messageReaction.partial) {
+            // If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
+            try {
+                await messageReaction.fetch();
+            } catch (error) {
+                console.error('Something went wrong when fetching the message: ', error);
+                // Return as `reaction.message.author` may be undefined/null
+                return;
+            }
+        }
+
+        logger.info("Message Reaction Add : " + messageReaction + "\n" + user.id);
+        // LOG
+        RoleReac.userReacAdd(pumpmymongoose,messageReaction,user)
+    });
+
+    //Role Reac messageReactionRemove
+    // if bot reaction removed, add again
+    bot.on("messageReactionRemove", async (messageReaction, user) => {
+
+        if (messageReaction.partial) {
+            // If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
+            try {
+                await messageReaction.fetch();
+            } catch (error) {
+                console.error('Something went wrong when fetching the message: ', error);
+                // Return as `reaction.message.author` may be undefined/null
+                return;
+            }
+        }
+
+        logger.info("Message Reaction Remove : " + messageReaction + "\n" + user.id);
+        // LOG
+        //RoleReac.userRemoveReac(pumpmymongoose,messageReaction,user)
+    });
+
+    //Role Reac messageReactionRemoveAll
+    // if bot reaction removed, add again
+    bot.on("messageReactionRemoveAll", async (message) => {
+
+        if (message.partial) {
+            // If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
+            try {
+                await message.fetch();
+            } catch (error) {
+                console.error('Something went wrong when fetching the message: ', error);
+                // Return as `reaction.message.author` may be undefined/null
+                return;
+            }
+        }
+
+        logger.info("Message Reaction Remove All : " + message);
+        // LOG
+        //RoleReac.messageRemoveAllReac(pumpmymongoose,message)
+    });
+
+    //Role Reac messageReactionRemoveEmoji
+    // if bot reaction removed, add again
+    bot.on("messageReactionRemoveEmoji", async (messageReaction) => {
+        
+        if (messageReaction.partial) {
+            // If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
+            try {
+                await messageReaction.fetch();
+            } catch (error) {
+                console.error('Something went wrong when fetching the message: ', error);
+                // Return as `reaction.message.author` may be undefined/null
+                return;
+            }
+        }
+        
+        logger.info("Message Reaction Remove Emoji : " + messageReaction);
+        // LOG
+        //RoleReac.messageRemoveEmojiReac(messageReaction)
+    });
+
+    //Role Reac messageDelete
+    // if message delete, warn admin & remove message data
+
     // events deduplicator module
     const EventsDeduplicator = require('./libs/events_deduplicator');
     EventsDeduplicator.setLogger(logger);
@@ -83,5 +181,15 @@ function setupBot(config,pumpmymongoose,bot) {
             });
         });        
     });
+
+    //Commands setup
+    client.commands = new Discord.Collection();
+    const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+    for (const file of commandFiles) {
+	    const command = require(`./commands/${file}`);
+        client.commands.set(command.name, command);
+        logger.info("CommandsManager : command \"" +command.name + "\" added")
+    }
 
 }

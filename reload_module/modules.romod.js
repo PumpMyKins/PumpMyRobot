c47cb2d45eeddc5bbd818e41e4e5d96ce284cb64
Clem Fern <clemfern456@gmail.com>
2021-06-27 13:23:41 +0200

    built in module code clean up

--- reload_module/modules.romod.js
+++ reload_module/modules.romod.js
@@ -4,14 +4,13 @@ export default {
     name: "builtin_MODULE",
     intents: ["GUILD_PRESENCES", "GUILD_MEMBERS"],
     load(manager) {
-        manager.LOGGER.info("Loading RELOAD module : add module command...");
+        manager.LOGGER.info("Loading : add module command...");
 
         ReloadCmd.permissions = getConfigPermissions(); // SET PERMISSION
-        console.log(ReloadCmd);
         manager.CMD.register(ReloadCmd); // REGISTER COMMAND
     },
     unload(manager) {
-        manager.LOGGER.info("Unloading RELOAD module : remove module command...");
+        manager.LOGGER.info("Unloading : remove module command...");
         manager.CMD.unregister(ReloadCmd); // UNREGISTER COMMAND
     }
 }
